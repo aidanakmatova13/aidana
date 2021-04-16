16e33fd0ad80e76f68f03160ec18f55ef2c43d6a
aidanakmatova13 <79746162+aidanakmatova13@users.noreply.github.com>
2021-04-16 13:10:30 +0600

    Add files via upload

---
+++ main.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html style="background: url(https://images.squarespace-cdn.com/content/v1/534eb383e4b013cdd4cd17a8/1398471158153-1S09IRJODCDYTDTODGPY/ke17ZwdGBToddI8pDm48kPhE_b-FKx_EcxRx4teFEVN7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k5fwC0WRNFJBIXiBeNI5fL8LmMajxsBXeYxVzkYts3ds68Ud4HgM4ArFxmxGpI5hQ/background-final_web.jpg?format=2500w);"> 
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title></title>
+</head>
+
+<style>
+  .fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+
+h2{
+  color: black;
+}
+
+}
+</style>
+<body>
+
+
+<h2><center>HI, WELCOME TO MY PORTFOLIO!</center></h2>
+<h3>Web developer from Bishkek!</h3>
+<h3>Name: AIDANA AKMATOVA</h3>
+<h3>Age: 19</h3>
+<p>.</p>
+<p>.</p>
+<p>.</p>
+<p>.</p>
+<p>.</p>
+<p>.</p>
+<p>.</p>
+<p>.</p>
+<h3>Me in social media</h3>
+
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+
+</body>
+</html>
+
